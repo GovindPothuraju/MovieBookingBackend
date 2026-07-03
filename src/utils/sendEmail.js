8ccac22fa2e68463ac1e7dce5225c1ddc60c72e5
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-07-03 14:41:07 +0530

    profile updated

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,27 +1,46 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === "true", // true for 465, false for 587
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+const axios = require("axios");
 
 const sendEmail = async ({ to, subject, html, text }) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"${process.env.SMTP_FROM_NAME}" <${process.env.SMTP_FROM_EMAIL}>`,
-      to,
-      subject,
-      text,
-      html,
-    });
-    return info;
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: process.env.SMTP_FROM_NAME,
+          email: process.env.SMTP_FROM_EMAIL,
+        },
+
+        to: [
+          {
+            email: to,
+          },
+        ],
+
+        subject,
+
+        htmlContent: html,
+
+        textContent: text || "",
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+        },
+      }
+    );
+
+    console.log("Email Sent Successfully");
+    console.log(response.data);
+
+    return response.data;
   } catch (err) {
-    console.error("Email Error:", err);
+    console.error(
+      "Brevo Error:",
+      err.response?.data || err.message
+    );
+
     throw err;
   }
 };
